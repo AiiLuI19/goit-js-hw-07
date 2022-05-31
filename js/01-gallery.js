d60f04a6673ec63a2ca2fc2443507f797da81686
Iuliia <iuliiaiavorskaia@gmail.com>
2022-05-31 17:23:11 +0300

    finish 7 HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Variables
+const galleryContainer = document.querySelector(".gallery");
+const picturesMarkup = creatGalleryMarkup(galleryItems);
+let instance; //змінна з бібліотеки
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+// Реализация делегирования
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+function creatGalleryMarkup(items) {
+return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" target="_self" href="${original}" >
+    <img
+      
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}).join('');
+}
+
+// 2.Реализация делегирования на div.gallery и получение url большого изображения.
+function onGalleryItemClick(evt) {
+  evt.preventDefault(); 
+
+  const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
+    return;
+  }
+  const url = evt.target.dataset.source; 
+  const alt = evt.target.alt;
+  openModalWindow(url, alt);
+} 
+
+// 3.  Открытие модального окна по клику на элементе галереи. 
+function openModalWindow(url,alt) {
+   instance = basicLightbox.create(`
+    <img 
+      src="${url}"
+      alt ="${alt}"
+    > 
+`)
+  instance.show();
+
+  window.addEventListener("keydown", closeModalWindow);
+}
+
+// 4*. Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно.
+//  У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+function closeModalWindow(evt) {
+  if (evt.key !== "Escape" ) {
+    return;
+  }
+  instance.close();
+  console.log(instance)
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// Variables
+const galleryContainer = document.querySelector(".gallery");
+const picturesMarkup = creatGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+// 1.Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону 
+// элемента галереи.Используй готовый код из первого задания.
+function creatGalleryMarkup(items) {
+return items.map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+        <img 
+        class="gallery__image" 
+        src="${preview}" 
+        alt="${description}"
+        title="${description}"/>
+    </a>`;
+}).join('');  
+}
+
+// 2.Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
+// Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// new SimpleLightbox('.some-element a', { /* options */ });
+let gallery = new SimpleLightbox('.gallery a');
+console.log(gallery);
+// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из 
+// атрибута alt.Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия 
+// изображения.
